b427f6d70874efd9a169cdbce1b0a8618ca85f87
nalydh <dylanhoang9@gmail.com>
2025-07-11 21:23:16 +0200

    Link submit button to POST

--- frontend/app/budget/page.jsx
+++ frontend/app/budget/page.jsx
@@ -13,13 +13,34 @@ function BudgetPage() {
   function handleChange(e) {
     setForm({
       ...form,
-      [e.target.name]: e.target.value,
+      [e.target.name]: Number(e.target.value),
     });
     console.log(form);
   }
 
-  function handleSubmit() {
-    console.log("Submitted!");
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("/budget/split", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          income: form.income,
+          needs_pct: form.needsPct,
+          wants_pct: form.wantsPct,
+          savings_pct: form.savingsPct,
+        }),
+      });
+
+      const data = await response.json();
+      console.log("Received split:", data);
+      // TODO: Store this in state to show user results
+    } catch (error) {
+      console.error("POST ERROR:", error);
+    }
   }
 
   return (
@@ -43,5 +64,4 @@ function BudgetPage() {
     </div>
   );
 }
-
 export default BudgetPage;
